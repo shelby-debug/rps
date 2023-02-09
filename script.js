37308e9d5d4e8b0a2a6d294bae9e9090ee3e4133
Shelby Henry <116368629+shelby-debug@users.noreply.github.com>
2023-02-08 21:40:11 -0500

    created computer choice

---
+++ script.js
@@ -0,0 +1,17 @@
+let r = "Rock";
+let p = "Paper";
+let s = "Scissors";
+let choice = Math.random();
+
+function getComputerChoice(choice) {
+  return choice}
+  console.log (choice)
+
+  if (choice < .333) {
+    alert (r)}
+
+  else if (choice > .666) {
+    alert (s)
+  }
+  else {
+    alert (s)}

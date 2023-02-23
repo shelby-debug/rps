4a76799eeb484473cab800b6c63d6ef07066fb67
shelby-debug <116368629+shelby-debug@users.noreply.github.com>
2023-02-22 20:15:04 -0500

    Revert "Add some comments for the booger."

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// NOTE: These variables are being defined a single time when this file is executed.
-// Importantly, that means that when you call `choice` later in this file, it will
-// only ever surface the value that was returned the first time.
 let choice = Math.random();
 let r = "rock";
 let p = "paper";
@@ -8,8 +5,6 @@ let s = "scissors";
 
 const computerSelection = choice
 
-// NOTE: This function is only spanning two lines, because of the `}` after 
-// `choice` below. Is that intentional?
 function getComputerChoice(choice) {
   return choice}
   console.log (choice)
@@ -25,18 +20,13 @@ function getComputerChoice(choice) {
 
 
 let playerSelection = (prompt ("Rock, Paper, or Scissors?")).toLowerCase()
-// I don't think that the variables below are being used?
 let rock = .1
 let paper= .5
 let scissors = .8
 console.log(playerSelection)
-
-// Similar to the above, this function only spans the following two lines because
-// of the closing `}` after `winner`. I would expect this to error out, since it
-// does not look like `winner` is being defined anywhere?
 function singleRound (playerSelection, computerSelection){
    return winner}
-  
+   
     if ((playerSelection == r && computerSelection > .666) || (playerSelection == p && computerSelection < .333) || (playerSelection == s && (computerSelection > .33 && computerSelection < .666) )) {
      alert ("You win!!!")}
    
@@ -47,8 +37,3 @@ function singleRound (playerSelection, computerSelection){
      alert ("It's a tie!")
     }
   console.log(computerSelection)
-
-// I'd recommend trying to break your functions out so that it is clear what they
-// are responsible for - for example if you have a function for "get computer choice", I'd
-// expect that it would generate it's random selection and _return_ the appropriate choice,
-// either as a string like "rock" or a numeric value `.1` like you are above.

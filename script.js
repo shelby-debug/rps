5aa31ded5db27ddd27f9a95ac72afdf18772b915
Shelby Henry <116368629+shelby-debug@users.noreply.github.com>
2023-02-22 20:08:31 -0500

    game function/loop

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+
+// NOTE: These variables are being defined a single time when this file is executed.
+// Importantly, that means that when you call `choice` later in this file, it will
+// only ever surface the value that was returned the first time.
 let choice = Math.random();
 let r = "rock";
 let p = "paper";
 let s = "scissors";
 
-const computerSelection = choice
+let computerSelection = choice
+
+
 
 function getComputerChoice(choice) {
-  return choice}
   console.log (choice)
 
   if (choice < .333) {
@@ -17,16 +22,16 @@ function getComputerChoice(choice) {
   }
   else {
     alert (p)}
+  }
 
 
 let playerSelection = (prompt ("Rock, Paper, or Scissors?")).toLowerCase()
-let rock = .1
-let paper= .5
-let scissors = .8
+
 console.log(playerSelection)
+
+
 function singleRound (playerSelection, computerSelection){
-   return winner}
-   
+    
     if ((playerSelection == r && computerSelection > .666) || (playerSelection == p && computerSelection < .333) || (playerSelection == s && (computerSelection > .33 && computerSelection < .666) )) {
      alert ("You win!!!")}
    
@@ -35,5 +40,25 @@ function singleRound (playerSelection, computerSelection){
 
     else {
      alert ("It's a tie!")
-    }
+
+    }}
   console.log(computerSelection)
+
+
+   
+ function game() {
+  let computerchoice = Math.random();
+  let playerSelection = (prompt ("Rock, Paper, or Scissors?")).toLowerCase()
+  for (let i = 0; i < 5; i++);
+  singleRound()
+ }
+  
+game()
+console.log(game)
+
+  
+
+// I'd recommend trying to break your functions out so that it is clear what they
+// are responsible for - for example if you have a function for "get computer choice", I'd
+// expect that it would generate it's random selection and _return_ the appropriate choice,
+// either as a string like "rock" or a numeric value `.1` like you are above.
